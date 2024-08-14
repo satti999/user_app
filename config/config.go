5abbf729c764b7911188dec00e1b8c891042839d
satti999 <samdanisati92@gmail.com>
2024-08-14 15:46:57 +0500

    testing the gitignore

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+type Configure struct {
+	GoogleLoginConfig oauth2.Config
+}
+
+var AppConfig Configure
+
+func GoogleConfig() oauth2.Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+	AppConfig.GoogleLoginConfig = oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		}, // Adjust scopes as needed
+		Endpoint: google.Endpoint,
+	}
+
+	return AppConfig.GoogleLoginConfig
+
+}
